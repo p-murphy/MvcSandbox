f0d11b543e4c0e39c1dbbb62947de4486c0de857
Patrick Murphy <patrick.watari@gmail.com>
2016-01-11 15:31:09 -0800

    Add Projects controller and views

---
+++ Controllers/Projects.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcSandbox.Controllers
+{
+    public class Projects : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AdvancedLearner()
+        {
+            return View();
+        }
+
+        public IActionResult CanvasWrite()
+        {
+            return View();
+        }
+
+        public IActionResult AnimalAdventure()
+        {
+            return View();
+        }
+
+        public IActionResult ArchitecturalVisualization()
+        {
+            return View();
+        }
+    }
+}
