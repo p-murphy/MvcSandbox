42c928c4f98854632d597a33302d25e5daec42c2
Patrick Murphy <patrick.watari@gmail.com>
2016-01-29 20:45:57 -0800

    Add snap.js

--- wwwroot/_references.js
+++ wwwroot/_references.js
@@ -2,6 +2,7 @@
 /// <reference path="../gulpfile.js" />
 /// <reference path="js/jquery-2.2.0.js" />
 /// <reference path="js/site.js" />
+/// <reference path="js/snap.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
